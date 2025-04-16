b46d5c18003e6b8f27276fcc42e459fc54d40317
ongzzzz <kyjj0506@naver.com>
2025-04-16 13:26:03 +0800

    Chore: eslint 속성 변경

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import React from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { version } from 'react-dom'
 
 export default [
   { ignores: ['dist'] },
@@ -17,11 +19,13 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
@@ -29,5 +33,10 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      }
+    }
   },
 ]
