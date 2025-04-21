1d72edd92be5bb8e34c76efa9ef591f513abbdde
ongzzzz <kyjj0506@naver.com>
2025-04-21 14:43:05 +0800

    Test: Vegetarian 페이지 라우팅

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 import Home from '../pages/home/Home';
+import Vegetarian from '../pages/about_books/Vegetarian';
+import VegetarianAni from '../components/about_books/VegetarianAni';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutbooks/vegetarian" element={<Vegetarian/>}/>
       </Routes>
     </BrowserRouter>
   );
