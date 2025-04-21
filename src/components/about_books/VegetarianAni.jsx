3b8dc2c63e35bcf0b6e4a0f8018b15929c9a5010
ongzzzz <kyjj0506@naver.com>
2025-04-21 14:44:03 +0800

    Design: 채식주의자 내부 애니메이션 컴포넌트 1차퍼블

---
+++ src/components/about_books/VegetarianAni.jsx
@@ -0,0 +1,34 @@
+import { motion } from "framer-motion";
+import "../../styles/about_books/VegetarianAni.scss";
+
+const messages = [
+  "나는 나무가 되고싶어.",
+  "나 고기가 너무 역겨워.",
+  "피가 싫어.",
+  "나는 나를 잃고 있다.",
+  "나는 존재하는가?",
+  "거지가 흔들린다.",
+  "나는 거부할 수 없는가?"
+];
+
+const VegetarianAni = () => {
+  return (
+    <div className="ani-container">
+      <div className="message-bubbles">
+        {messages.map((msg, idx) => (
+          <motion.div
+            key={idx}
+            className={`bubble bubble-${idx + 1}`}
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: idx * 0.4, duration: 0.8, ease: "easeOut" }}
+          >
+            {msg}
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default VegetarianAni;
