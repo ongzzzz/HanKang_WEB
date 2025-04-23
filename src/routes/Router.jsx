1cbe23de5e93600603f7a038292ded3a289e10b7
ongzzzz <kyjj0506@naver.com>
2025-04-23 14:01:28 +0800

    Test: 여수의 사랑 페이지 라우팅 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from '../pages/home/Home';
 import Vegetarian from '../pages/about_books/Vegetarian';
 import White from '../pages/about_books/White';
+import Yeosu from '../pages/about_books/Yeosu';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/aboutbooks/vegetarian" element={<Vegetarian/>}/>
         <Route path="/aboutbooks/white" element={<White />}/>
+        <Route path="/aboutbooks/yeosu" element={<Yeosu />}/>
       </Routes>
     </BrowserRouter>
   );
