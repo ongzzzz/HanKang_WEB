3c70b63dc824a3a7b217a6c94b4300ae9cf434b1
ongzzzz <kyjj0506@naver.com>
2025-04-24 14:17:38 +0800

    Feat: 소년이 온다 페이지 마우스 효과 추가

--- src/pages/about_books/Mayof.jsx
+++ src/pages/about_books/Mayof.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 import MayofWriter from "../../components/about_books/Mayof/MayofWriter";
 import MayofAni from "../../components/about_books/Mayof/MayofAni";
 import Button from "../../components/button/Button";
@@ -5,11 +8,35 @@ import Button from "../../components/button/Button";
 import "../../styles/about_books/Mayof/Mayof.scss";
 
 const Mayof = () => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePos({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
-    <div className="mayof-container">
+    <div 
+      className="mayof-container"
+      style={{
+        cursor: isOpen ? "default" : "none",
+      }}
+    >
       <Button />
       <MayofWriter />
-      <MayofAni />
+      <MayofAni isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      {!isOpen && (
+        <motion.div 
+          className="mouse-fire"
+          animate={{ x: mousePos.x, y: mousePos.y }}
+          transition={{ type: "spring", stiffness: 300, damping: 30 }}
+        />
+      )}
     </div>
   );
 };
