fe699bae6ff2f7912361e88166b3f8a3a2d50e6c
ongzzzz <kyjj0506@naver.com>
2025-04-30 16:29:43 +0800

    Design: Loading 컴포넌트 1차 퍼블

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "../../styles/loading/Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <img src="../../images/loading/loading 1.png" />
+    </div>
+  );
+};
+
+export default Loading;
