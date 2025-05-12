294df29e7afc226ac1400029ce6b164a1b178dc2
ongzzzz <kyjj0506@naver.com>
2025-05-12 18:11:08 +0800

    Feat: 메인 페이지 번역 기능 추가

--- src/components/button/Translate.jsx
+++ src/components/button/Translate.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import "../../styles/button/Translate.scss";
 
-const Translate = () => {
+const Translate = ({ children }) => {
   const [activeLang, setActiveLang] = useState("kr");
-  return (
-    <div className="translate-container">
-      <button className={`lang-btn ${activeLang === "kr" ? "active" : ""}`} onClick={() => setActiveLang("kr")}>KR</button>
-      <hr />
-      <button className={`lang-btn ${activeLang === "cn" ? "active" : ""}`} onClick={() => setActiveLang("cn")}>CN</button>
+  const content = children[activeLang];
+
+return (
+    <div className={`translate-wrapper lang-${activeLang}`}>
+      <div className="translate-container">
+        <button
+          className={`lang-btn ${activeLang === "kr" ? "active" : ""}`}
+          onClick={() => setActiveLang("kr")}
+        >
+          KR
+        </button>
+        <hr />
+        <button
+          className={`lang-btn ${activeLang === "cn" ? "active" : ""}`}
+          onClick={() => setActiveLang("cn")}
+        >
+          CN
+        </button>
+      </div>
+      <div className="translate-content">
+        <p dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
     </div>
   );
 };
